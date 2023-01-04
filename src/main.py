40465de8e2840cc653f09ddac4cac8256de081a3
Jakub Šenkýř <senkyr@spstrutnov.cz>
2023-01-04 10:40:16 +0100

    Pohyblivý čtvereček

--- src/main.py
+++ src/main.py
@@ -9,6 +9,10 @@ pygame.init()
 # vytvoreni okna (stanoveni rozliseni)
 okno = pygame.display.set_mode((800, 600))
 
+# pocatecni hodnoty
+pozice_x = 200
+pozice_y = 100
+
 # vykreslovaci smycka
 while True:
     
@@ -21,13 +25,17 @@ while True:
             # vypnuti aplikace
             sys.exit()
     
+    # pohyb ctverecku
+    pozice_x += 0.1
+    pozice_y += 0.1
+    
     # barva pozadi
     #           R    G    B
     okno.fill((255, 255, 255))
     
     # zobrazeni ctverecku
-    #                kde     barva      pozice   rozmery
-    pygame.draw.rect(okno, (0, 0, 0), (200, 100, 50, 50))
+    #                kde     barva           pozice        rozmery
+    pygame.draw.rect(okno, (0, 0, 0), (pozice_x, pozice_y, 50, 50))
     
     # refresh zobrazeni
     pygame.display.update()

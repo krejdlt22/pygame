3cb3f6a9ac26af01ad50dd993d2cc4a36a72e5a6
Jakub Šenkýř <senkyr@spstrutnov.cz>
2023-01-04 10:32:31 +0100

    Aplikaci lze zavřít

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+# pouzite knihovny
+import sys
+
 # pouzity framework
 import pygame
 # spusteni frameworku
@@ -6,9 +9,21 @@ pygame.init()
 # vytvoreni okna (stanoveni rozliseni)
 okno = pygame.display.set_mode((800, 600))
 
-# barva pozadi
-#           R    G    B
-okno.fill((255, 255, 255))
+# vykreslovaci smycka
+while True:
+    
+    # projdeme si vsechny udale udalosti
+    for udalost in pygame.event.get():
+        # nastala udalost vypnuti aplikace
+        if udalost.type == pygame.QUIT:
+            # vypnuti frameworku
+            pygame.quit()
+            # vypnuti aplikace
+            sys.exit()
+    
+    # barva pozadi
+    #           R    G    B
+    okno.fill((255, 255, 255))
 
-# refresh zobrazeni
-pygame.display.update()
+    # refresh zobrazeni
+    pygame.display.update()
